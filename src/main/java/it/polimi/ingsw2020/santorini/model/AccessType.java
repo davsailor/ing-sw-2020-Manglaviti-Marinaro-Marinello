6cea0df43438ab79f2c154636a40714b5eb87973
LukeMari <luca.marinello@polimi.it>
2020-03-29 03:00:04 +0200

    Class of the Enum AccessType has been updated

--- src/main/java/it/polimi/ingsw2020/santorini/model/AccessType.java
+++ src/main/java/it/polimi/ingsw2020/santorini/model/AccessType.java
@@ -1,5 +1,12 @@
 package it.polimi.ingsw2020.santorini.model;
 
+/**
+ * this enum represents the status of the cells:
+ * Occupied if there is a builder in it
+ * Free in the opposite case
+ * Forbidden in the case the cell represents the limit of the board
+ * Dome if it is a dome built in the cell
+ */
 public enum AccessType {
-    Occupied, Free, Forbidden;
+    Occupied, Free, Forbidden, Dome;
 }
